c9fbbb91e914ad7d2680da09d69463bf1ec0902d
Qianyue-hub <1635765811@qq.com>
2021-05-22 10:50:11 +0800

    Debug调试代码

---
+++ Debug/test.c
@@ -0,0 +1,174 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+//死循环调试
+//F5 - 启动调试-和F9配合使用
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehe\n");
+//		arr[i] = 0;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", 10 - i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	{
+//		int tmp = 0;
+//		printf("tmp = %d\n", tmp);
+//
+//	}
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	return 0;
+//}
+
+//多层调用
+//void test2()
+//{
+//	printf("hehe\n");
+//}
+//
+//void test1()
+//{
+//	test2();
+//}
+//
+//void test()
+//{
+//	test1();
+//}
+//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+//阶乘调试
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;//保存最终结果
+//	int n = 0;
+//	int ret = 1;//保存n的阶乘
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		ret = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//1.栈区的默认使用：先使用高地址处的空间，再使用低地址处的空间
+//2.数组随着下标的增长地址是由低到高变化
+//死循环调试越界
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehe\n");
+//		arr[i] = 0;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//void my_strcpy(char* dest,char* src)
+//{
+//	while (*src != '\0')
+//	{
+//		*dest = *src;
+//		src++;
+//		dest++;
+//	}
+//	*dest = *src;
+//}
+
+//my_strcpy(char* dest,const char* src)
+//{
+//	char* ret = dest;
+//	assert(dest != NULL);//断言
+//	assert(src != NULL);//断言
+//	//把src指向的字符串拷贝到的dest指向的空间，包含'\0'字符
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	//strcpy
+//	//字符串拷贝
+//	char arr1[] = "###############";
+//	char arr2[] = "bit";
+//	printf("%s\n", my_strcpy(arr1, arr2));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 10;
+//	const int* p = &num;
+//	//const放在指针变量的*左边时，修饰的是*p，不能通过p来改变*p(num)的值
+//	//const放在指针变量的*右边时，修饰的是指针变量p本身，p不能被改变了
+//	*p = 20;
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+int my_strlen(const char *str)
+{
+	int count = 0;
+	assert(str != NULL);//保证指针的有效性
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+
+int main()
+{
+	char arr[] = "abcdef";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+	return 0;
+}
